834de5681ede84c317396f5ea9cf1c5f70fd895a
Al-Julanda <44.hinai@gmail.com>
2024-06-29 11:58:02 +0400

    Update integer or float.py

--- integer or float.py
+++ integer or float.py
@@ -1,7 +1,6 @@
 x = float(input("Enter a number: "))
-if x % 2 == 0:
-	print("The number is an integer! ")
-elif x % 2 == 1: 
-	print("The number is an integer! ")
+
+if x.is_integer():
+    print("The number is an integer!")
 else:
-	print("The number is a float! ")
+    print("The number is a float!")
