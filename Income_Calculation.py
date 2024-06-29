2096d5943158882e9a0b7e63eba24f4934e15949
Al-Julanda <44.hinai@gmail.com>
2024-06-29 17:04:21 +0400

    Update Income_Calculation.py

--- Income_Calculation.py
+++ Income_Calculation.py
@@ -2,5 +2,5 @@ income = float(input("Enter your income: "))
 tax_rate = float(input("Enter the tax rate: "))
 my_taxes = income * tax_rate
 net_income = income - my_taxes 
-print(net_income) 
+print(f"Your net income is {net_income}") 
 print(type(net_income))
